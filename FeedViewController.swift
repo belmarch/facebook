85abd705b19faa34eaf03d81376b276b44ede624
Ron Belmarch <belmarch@yahoo.com>
2014-09-05 00:20:34 -0700

    Scrollable views
    Each view controller should have an image view for the custom
    navigation bar and a scroll view that contains an image view for the
    content of the tab. Hide the navigation bar of each navigation
    controller.
    Since you have to set the content size of the scroll view in code, you
    have to create a custom view controller for each screen (e.g.,
    FeedViewController, MessagesViewController, etc)
    For the FeedViewController, add an additional image view that contains
    the compose buttons for status, photos, and check ins.

---
+++ FeedViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FeedViewController.swift
+//  facebook
+//
+//  Created by Ron Belmarch on 9/4/14.
+//  Copyright (c) 2014 Belmerica. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        scrollView.contentSize = CGSize(width: 320, height: 1025)
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
